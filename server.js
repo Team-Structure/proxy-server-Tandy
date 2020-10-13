06ed8b6e6c5822228d2f77ef09b467acc8f2bf4f
Tandy Nguyen <nguyetan001@gmail.com>
2020-10-13 01:18:20 -0400

    Add endpoint to manage api requests

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const path = require('path');
+const { checkApiUrl } = require('./middleware/api');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, './public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.use('/api', checkApiUrl, (req, res) => {
+  fetch(req.serviceUrl)
+    .then((fetchRes) => {
+      if (!fetchRes.ok) {
+        res.status(fetchRes.status).send(fetchRes.statusText);
+        return;
+      }
+      return fetchRes.json();
+    })
+    .then((fetchData) => res.json(fetchData))
+    .catch((err) => res.status(400).send('Bad Request. Invalid URL.'));
+});
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 const PORT = 3000;
