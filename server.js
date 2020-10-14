5cc5b93b3548574d160eb6a666e1e85f4dc907af
Tandy Nguyen <nguyetan001@gmail.com>
2020-10-13 23:32:08 -0400

    Revert back to apps handling api requests

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
-const { checkApiUrl } = require('./middleware/api');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, './public')));
-
-app.use('/api', checkApiUrl, (req, res) => {
-  fetch(req.serviceUrl)
-    .then((fetchRes) => {
-      if (!fetchRes.ok) {
-        res.status(fetchRes.status).send(fetchRes.statusText);
-        return;
-      }
-      return fetchRes.json();
-    })
-    .then((fetchData) => res.json(fetchData))
-    .catch((err) => res.status(400).send('Bad Request. Invalid URL.'));
-});
 
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname, '/public/index.html'));
